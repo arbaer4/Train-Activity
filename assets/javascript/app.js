4c5b85b4687931437082638e0e1c533952f12628
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-06-12 10:33:47 -0400

    HTML started, bootstrap, jquery, firebase linked

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA7xEi3T554fZefn4yVUd4uNJ3boDten1Y",
+    authDomain: "train-activity-1e8ad.firebaseapp.com",
+    databaseURL: "https://train-activity-1e8ad.firebaseio.com",
+    projectId: "train-activity-1e8ad",
+    storageBucket: "train-activity-1e8ad.appspot.com",
+    messagingSenderId: "508210309256",
+    appId: "1:508210309256:web:9e429bf518d20f72"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Create a variable to reference the database
+  var database = firebase.database();
