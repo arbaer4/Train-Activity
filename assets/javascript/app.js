8ea7b797b384b693e24b8ba5a4d82bb385a46a32
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-06-12 22:10:15 -0400

    firebase event added, train data row populating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,3 +13,86 @@
 
   // Create a variable to reference the database
   var database = firebase.database();
+  var trainName;
+  var trainDestination;
+  var trainFrequency; 
+  var firstTrain;
+  var trainNextArrival;
+  var trainMinutesAway;
+
+
+$("button").on("click", function(event){
+  //prevent form from reloading
+event.preventDefault(event)
+trainName = $("#trainname").val().trim();
+trainDestination = $("#destination").val().trim();
+firstTrain = $("#traintime").val().trim();
+trainFrequency = $("#frequency").val().trim();
+
+
+//console log values
+console.log(trainName);
+console.log(trainDestination);
+console.log(firstTrain);
+console.log(trainFrequency);
+
+//upload train data to database
+database.ref().push({
+  dbTrainName: trainName,
+  dbTrainDestination: trainDestination, 
+  dbFirstTrain: firstTrain,
+  dbTrainFrequency: trainFrequency
+})
+
+//alert train is added
+alert("Train successfully added");
+
+//Clears all text boxes
+$("#trainname").val("");
+$("#destination").val("");
+$("#traintime").val("");
+$("#frequency").val("");
+  });
+
+//create firebase event for adding train to the database and a row in the html when submitted
+
+database.ref().on("child_added", function(childSnapshot){
+  console.log(childSnapshot.val());
+  //store everything into a variable
+
+  var trainName = childSnapshot.val().dbTrainName;
+  var trainDestination = childSnapshot.val().dbTrainDestination;
+  var firstTrain = childSnapshot.val().dbFirstTrain;
+  var trainFrequency = childSnapshot.val().dbTrainFrequency;
+
+  //train info
+  console.log(trainName);
+  console.log(trainDestination);
+  console.log(firstTrain);
+  console.log(trainFrequency);
+
+  //calculate the train time from military to local time format
+  // var convertedTime = moment.tz.setDefault(timeStart, "HH:mm").subtract(1, "years");
+  // console.log(convertedTime);
+
+  // var currentTime = moment.tz.setDefault();
+
+
+
+
+
+
+
+//create a new row
+var newRow = $("<tr>").append(
+  $("<td>").text(childSnapshot.val().dbTrainName),
+  $("<td>").text(childSnapshot.val().dbTrainDestination),
+  $("<td>").text(childSnapshot.val().dbTrainFrequency),
+  $("<td>").text(childSnapshot.val().dbFirstTrain)
+  
+);
+//append the new row to the table
+$("#traintable > tbody").append(newRow);
+
+
+});
